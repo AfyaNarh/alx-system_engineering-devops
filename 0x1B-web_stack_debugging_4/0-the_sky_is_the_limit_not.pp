005d27ededdca357e3b67e60ab6a3a8485c82e71
AfyaNarh <nhyiraafya1@gmail.com>
2023-12-22 11:31:09 +0000

    fixing errors

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,13 @@
-# Fixing the number of failed requests to 0
+# The amount of traffic an Nginx server can handle is increased.
+
+# The ULIMIT of the default file is increased
 exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+}
+#->
+# Restart the Nginx
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
 }
